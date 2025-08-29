d53b62226129900cdb9834244eec7a9f692dfe67
rahul-kumar620 <rahulkumarjsrbhu@gmail.com>
2025-08-29 14:20:58 +0530

    product-type function done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
-// import React from "react";
+// // import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// // import Header from "./components/Header";
+// import HomeSlider from "./Pages/Home/HomeSlider";
+// import About from "./Pages/About/About";
+// import Boys from "./Pages/Boys/Boys";
+// import Girls from "./Pages/Girls/Girls";
+// import Mens from "./Pages/Mens/Mens";
+// import Women from "./Pages/Womens/Womens";
+// import Footer from "./components/Footer";
+// import Headers from "./components/Headers";
+// import Login from "./Pages/Login/Login";
+// import ProductDetails from "./components/ProductDetails";
+// import Favorites from "./components/Favorites";
+// import CartPages from "./components/CartPages";
+
+// function App() {
+//   return (
+//     <Router basename="/ecommerce">
+//       {/* <Header /> */}
+//       <Headers />
+//       <Routes>
+//         <Route path="/" element={<HomeSlider />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/boys" element={<Boys />} />
+//         <Route path="/girls" element={<Girls />} />
+//         <Route path="/mens" element={<Mens />} />
+//         <Route path="/womens" element={<Women />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/Productdetails/:id" element={<ProductDetails />} />
+//         <Route path="/favorites" element={<Favorites />} />
+//         <Route path="/cartpages" element={<CartPages />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
 import HomeSlider from "./Pages/Home/HomeSlider";
 import About from "./Pages/About/About";
 import Boys from "./Pages/Boys/Boys";
@@ -16,23 +54,26 @@ import CartPages from "./components/CartPages";
 
 function App() {
   return (
-    <Router basename="/ecommerce">
-      {/* <Header /> */}
-      <Headers />
-      <Routes>
-        <Route path="/" element={<HomeSlider />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/boys" element={<Boys />} />
-        <Route path="/girls" element={<Girls />} />
-        <Route path="/mens" element={<Mens />} />
-        <Route path="/womens" element={<Women />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Productdetails/:id" element={<ProductDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/cartpages" element={<CartPages />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Router basename="/ecommerce">
+        <Headers />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomeSlider />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/boys" element={<Boys />} />
+            <Route path="/girls" element={<Girls />} />
+            <Route path="/mens" element={<Mens />} />
+            <Route path="/womens" element={<Women />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/Productdetails/:id" element={<ProductDetails />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/cartpages" element={<CartPages />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
--- src/Pages/Mens/Mens.jsx
+++ src/Pages/Mens/Mens.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleLike } from "../../Redux/slices/LikeSlice";
+
 import "./Mens.scss";
-import axios from "axios";
+import { IoEyeOutline, IoHeartOutline, IoHeartSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Mens = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [filteredVisible, setFilteredVisible] = useState(false);
   const [openDropdown, setOpenDropdown] = useState(null);
-  const [data, setData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("shirts");
 
   // filter karege type ke anusar se uske liye ye hai
   const [selectedPrices, setSelectedPrices] = useState([]);
@@ -14,21 +18,19 @@ const Mens = () => {
   // const [selectedSizes, setSelectedSizes] = useState([]);
   // const [selectedColors, setSelectedColors] = useState([]);
 
-  const API = "http://localhost:8080/product";
+  const dispatch = useDispatch();
+  const likedItems = useSelector((state) => state.liked);
+  const navigate = useNavigate();
 
-  const getProductData = async () => {
-    try {
-      const res = await axios.get(API);
-      setData(res.data);
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-    }
+  // first hamko handle redux se karna hai
+  const handleLike = (section, AllmensClothes) => {
+    const likeKey = `${section}-${AllmensClothes.id}`;
+    dispatch(toggleLike({ key: likeKey, product: AllmensClothes }));
   };
 
-  useEffect(() => {
-    getProductData();
-  }, []);
+  const handleView = (product) => {
+    navigate(`/Productdetails/${product.id}`, { state: { product } });
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -55,6 +57,171 @@ const Mens = () => {
     setOpenDropdown(openDropdown === section ? null : section);
   };
 
+  //  product section
+  const AllmensClothes = {
+    shirts: [
+      {
+        id: 1,
+        name: "Elegant Straight Cotton Kurti",
+        title: "Women’s Casual Straight Kurti with Side Slits",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031739/EXCG00014CARBON_1_ciljac.webp",
+        price: "₹500",
+        span: "₹150",
+        inStock: true,
+        type: "T-Shirt",
+      },
+      {
+        id: 2,
+        name: "Floral Print A-Line Kurti",
+        title: "Rayon A-Line Kurti with Floral Pattern and 3/4 Sleeves",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031739/EXCG00014CARBON_1_ciljac.webp",
+        price: "₹700",
+        span: "₹250",
+        inStock: false,
+        type: "Shirt",
+      },
+      {
+        id: 3,
+        name: "Classic Printed Cotton Kurti",
+        title: "Ethnic Straight Kurti with Block Print Design",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031848/MFL00019MOUNTAIN_1_rlzvpz.webp",
+        price: "₹600",
+        span: "₹350",
+        type: "Jeanse",
+      },
+      {
+        id: 4,
+        name: "Elegant Kurti with Embroidery",
+        title: "Designer Kurti with Intricate Thread Work",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031848/MFL00019MOUNTAIN_1_rlzvpz.webp",
+        price: "₹700",
+        span: "₹300",
+      },
+      {
+        id: 5,
+        name: "Block Print Cotton Kurti",
+        title: "Women’s Casual Block Print Straight Kurti",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031866/SMCH00003EBONY_1_vuewpe.webp",
+        price: "₹400",
+        span: "₹350",
+      },
+      {
+        id: 6,
+        name: "Comfort Fit Rayon Kurti",
+        title: "Lightweight Rayon Kurti for Daily Wear",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031848/MFL00019MOUNTAIN_1_rlzvpz.webp",
+        price: "₹600",
+        span: "₹500",
+      },
+      {
+        id: 7,
+        name: "Elegant Printed Kurti",
+        title: "Stylish Kurti with Allover Print & Button Detailing",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031868/SMLW00002BEIGE_1_lavn27.webp",
+        price: "₹500",
+        span: "₹400",
+      },
+      {
+        id: 8,
+        name: "Traditional Cotton Kurti",
+        title: "Classic Cotton Kurti with Embroidered Yoke",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031871/SMLW00004DRYGRASS_1_ff03ed69-2678-4728-a04c-c336e9a2d4e4_x0dnkp.webp",
+        price: "₹600",
+        span: "₹400",
+      },
+    ],
+    tshirt: [
+      {
+        id: 9,
+        name: "Winter Velvet Kurti",
+        title: "Women’s Velvet Kurti with Full Sleeves",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1756031875/MSK00130ASPENGREEN_1_rffuop.webp",
+        price: "₹500",
+        span: "₹400",
+      },
+      {
+        id: 10,
+        name: "Festive Embroidered Kurti",
+        title: "Embroidered Anarkali Kurti for Festive Occasions",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹500",
+        span: "₹400",
+      },
+      {
+        id: 11,
+        name: "Denim Style Kurti",
+        title: "Trendy Denim Kurti with Button Front",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹600",
+        span: "₹400",
+      },
+      {
+        id: 12,
+        name: "Elegant Print Kurti",
+        title: "Casual Printed Kurti with Straight Fit",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹500",
+        span: "₹450",
+      },
+    ],
+    jeanse: [
+      {
+        id: 13,
+        name: "Simple Cotton Kurti",
+        title: "Basic Cotton Kurti for Everyday Wear",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹1000",
+        span: "₹900",
+      },
+      {
+        id: 14,
+        name: "Water-Resistant Kurti",
+        title: "Kurti with Water-Repellent Finish for Outdoor Use",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹1100",
+        span: "₹1000",
+      },
+      {
+        id: 15,
+        name: "Graphic Printed Kurti",
+        title: "Modern Kurti with Stylish Graphic Prints",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹1300",
+        span: "₹1200",
+      },
+      {
+        id: 16,
+        name: "Comfort Fit Cotton Kurti",
+        title: "Breathable Cotton Kurti for Casual Days",
+        image:
+          "https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp",
+        price: "₹900",
+        span: "₹750",
+      },
+    ],
+  };
+
+  const allMensproduct = [
+    ...AllmensClothes.shirts,
+    ...AllmensClothes.tshirt,
+    ...AllmensClothes.jeanse,
+  ];
+
   // ye price range hai
   const priceRanges = [
     { label: "₹0 - ₹500", min: 0, max: 500 },
@@ -62,28 +229,24 @@ const Mens = () => {
     { label: "₹1000+", min: 1000, max: Infinity },
   ];
 
-  const filterByPrice = (price) => {
-    if (selectedPrices.length === 0) return true;
-    return selectedPrices.some((label) => {
-      const range = priceRanges.find((r) => r.label === label);
-      return price >= range.min && price < range.max;
-    });
+  // ham catagari ke function start kar rahe hai
+
+  const handleBoysCatagory = (boyscatogory) => {
+    setSelectedCategory(boyscatogory.toLowerCase());
   };
 
-  const filterByType = (title) => {
+  //  Catagery section
+
+  const categoryProducts =
+    selectedCategory == "all"
+      ? allMensproduct
+      : AllmensClothes[selectedCategory] || [];
+
+  // ab ham filter karenge jaise ki shirt tshirt ke liye
+  const typedFilteredProducts = categoryProducts.filter((product) => {
     if (selectedTypes.length === 0) return true;
-    return selectedTypes.some((type) =>
-      title.toLowerCase().includes(type.toLowerCase())
-    );
-  };
-
-  const filterBySize = () => {
-    return true;
-  };
-
-  const filterByColor = () => {
-    return true;
-  };
+    return selectedTypes.includes(product.type);
+  });
 
   return (
     <>
@@ -95,68 +258,35 @@ const Mens = () => {
           <div className="boys-all-category">
             <p className="categoury-text">Shop by Category</p>
             <div className="categoury-all-boys">
-              <div className="boys-categoury-image-section">
+              <div
+                className="boys-categoury-image-section"
+                onClick={() => handleBoysCatagory("shirts")}
+              >
                 <img
                   className="boys-iamge-circle"
                   src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524927/bblobmsjbxnkphukd0mx_375x350_crop_center_x4cdcl.webp"
                 />
-                <p>Shirt</p>
+                <p>MENS</p>
               </div>
-              <div className="boys-categoury-image-section">
+              <div
+                className="boys-categoury-image-section"
+                onClick={() => handleBoysCatagory("tshirt")}
+              >
                 <img
                   className="boys-iamge-circle"
                   src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
                 />
-                <p>Shirt</p>
+                <p>BOYS</p>
               </div>
-              <div className="boys-categoury-image-section">
+              <div
+                className="boys-categoury-image-section"
+                onClick={() => handleBoysCatagory("jeanse")}
+              >
                 <img
                   className="boys-iamge-circle"
                   src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
                 />
-                <p>Shirt</p>
-              </div>
-              <div className="boys-categoury-image-section">
-                <img
-                  className="boys-iamge-circle"
-                  src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
-                />
-                <p>Shirt</p>
-              </div>
-              <div className="boys-categoury-image-section">
-                <img
-                  className="boys-iamge-circle"
-                  src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
-                />
-                <p>Shirt</p>
-              </div>
-              <div className="boys-categoury-image-section">
-                <img
-                  className="boys-iamge-circle"
-                  src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
-                />
-                <p>Shirt</p>
-              </div>
-              <div className="boys-categoury-image-section">
-                <img
-                  className="boys-iamge-circle"
-                  src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
-                />
-                <p>Shirt</p>
-              </div>
-              <div className="boys-categoury-image-section">
-                <img
-                  className="boys-iamge-circle"
-                  src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
-                />
-                <p>Shirt</p>
-              </div>
-              <div className="boys-categoury-image-section">
-                <img
-                  className="boys-iamge-circle"
-                  src="https://res.cloudinary.com/dqprmy5ro/image/upload/v1751524928/myfuhyrk9pb2ww11tb9w_770x700_crop_center_kfwcqw.webp"
-                />
-                <p>Shirt</p>
+                <p>KIDS</p>
               </div>
             </div>
           </div>
@@ -176,7 +306,7 @@ const Mens = () => {
                       className="left-side-texts-men"
                       onClick={() => toggleDropdown("productType")}
                     >
-                      Product Type {openDropdown === "productType" ? "▲" : "▼"}
+                      Product Type {openDropdown === "productType" ? "▼" : "▲"}
                     </p>
                     {openDropdown === "productType" && (
                       <div className="dropdown-content-men open">
@@ -198,27 +328,27 @@ const Mens = () => {
                         <label>
                           <input
                             type="checkbox"
-                            checked={selectedTypes.includes("Shirt")}
+                            checked={selectedTypes.includes("T-Shirt")}
                             onChange={() =>
                               setSelectedTypes((prev) =>
-                                prev.includes("Shirt")
-                                  ? prev.filter((t) => t !== "Shirt")
-                                  : [...prev, "Shirt"]
+                                prev.includes("T-Shirt")
+                                  ? prev.filter((t) => t !== "T-Shirt")
+                                  : [...prev, "T-Shirt"]
                               )
                             }
                           />
-                          Pants
+                          T-Shirts
                         </label>
 
                         <label>
                           <input
                             type="checkbox"
-                            checked={selectedTypes.includes("Shirt")}
+                            checked={selectedTypes.includes("Jeanse")}
                             onChange={() =>
                               setSelectedTypes((prev) =>
-                                prev.includes("Shirt")
-                                  ? prev.filter((t) => t !== "Shirt")
-                                  : [...prev, "Shirt"]
+                                prev.includes("Jeanse")
+                                  ? prev.filter((t) => t !== "Jeanse")
+                                  : [...prev, "Jeanse"]
                               )
                             }
                           />
@@ -320,29 +450,48 @@ const Mens = () => {
           {/* filter section end*/}
           {/* product image section */}
           <div className="all-product-image-section">
-            {data
-              .filter(
-                (item) =>
-                  filterByPrice(item.price) &&
-                  filterByType(item.title) &&
-                  filterBySize(item) &&
-                  filterByColor(item)
-              )
-              .map((item) => (
-                <div className="product-card" key={item.id}>
-                  <div className="image-section-all-product">
-                    <img
-                      src={item.imageUrls}
-                      alt={item.name}
-                      className="product-main-image"
-                    />
-                  </div>
-                  <div className="product-all-text">
-                    <p className="product-title-mens">{item.name}</p>
-                    <p className="product-price-mens">₹{item.price}</p>
+            {typedFilteredProducts.map((AllmensClothes) => (
+              <div key={AllmensClothes.id} className="all-content-mens-section">
+                <div className="image-main-mens-container">
+                  <img
+                    src={AllmensClothes.image}
+                    alt={AllmensClothes.name}
+                    className="product-image"
+                  />
+                  <div className="overlay_buttons-mens">
+                    <button
+                      className="like_btn-mens"
+                      onClick={() => handleLike("mens", AllmensClothes)}
+                    >
+                      {likedItems[`mens-${AllmensClothes.id}`] ? (
+                        <IoHeartSharp className="text-red-500 " />
+                      ) : (
+                        <IoHeartOutline className="like_btn-mens" />
+                      )}
+                    </button>
+                    {/* <button className="buy_now_btn-women">Buy Now</button> */}
+                    {AllmensClothes.inStock ? (
+                      <button className="buy_now_btn in-stock">In Stock</button>
+                    ) : (
+                      <button className="buy_now_btn out-of-stock">
+                        Out of Stock
+                      </button>
+                    )}
+                    <button
+                      className="view_btn-mens"
+                      onClick={() => handleView(AllmensClothes)}
+                    >
+                      <IoEyeOutline className="view_btn-mens" />
+                    </button>
                   </div>
                 </div>
-              ))}
+                <h3 className="product-boys-name">{AllmensClothes.name}</h3>
+                <div className="combine-price-or-span">
+                  <p className="price-product-mens">{AllmensClothes.price}</p>
+                  <p className="span-price-mens">{AllmensClothes.span}</p>
+                </div>
+              </div>
+            ))}
           </div>
 
           {/* product image section end */}
